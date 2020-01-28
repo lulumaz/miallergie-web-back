6c4fbdc2b33bf57993176d0a963f936e4da1b55e
Lucas Mazel <lucas.mazel@g-mail.fr>
2020-01-28 15:15:27 +0100

    dev : routing apk & web

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const express = require('express');
+const fs = require('fs');
 
+const app = express();
+
+app.set('view engine', 'ejs');
+
+
+//page accuil
+app.get('/', (req, res) => {
+     res.redirect('/index.html');
+});
+app.get('/index.html', (req, res) => {
+    res.render('./index.ejs');
+});
+
+
+//apk route
+app.get('/apk/release.apk', (req, res) => {
+    res.sendFile(__dirname+'/www/apk/release.apk');
+});
+
+
+app.get('/authentication', (req, res) => {
+    res.sendFile(__dirname+'/www/web/index.html');
+});
+
+app.use(express.static('www/web/'));
+
+
+app.listen(80, () => {
+    console.log(`Server started on port 80`);
+});
